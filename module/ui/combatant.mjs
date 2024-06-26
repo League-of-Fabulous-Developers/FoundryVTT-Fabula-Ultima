d2e8b0fd461192bc66306d0b90339e972eebc805
Sören Jahns <jahns.simon@web.de>
2024-06-26 21:28:12 +0200

    fix: Don't allow Tokens without Actor to be added to experimental combat

--- module/ui/combatant.mjs
+++ module/ui/combatant.mjs
@@ -1,6 +1,13 @@
 import { FRIENDLY, HOSTILE } from './combat.mjs';
 import { NpcDataModel } from '../documents/actors/npc/npc-data-model.mjs';
 
+Hooks.on('preCreateCombatant', function (document, data, options, userId) {
+	if (document instanceof FUCombatant && document.actorId === null) {
+		ui.notifications.info('FU.CombatTokenWithoutActor', { localize: true });
+		return false;
+	}
+});
+
 export class FUCombatant extends Combatant {
 	/**
 	 * @return {"friendly" | "hostile"}
