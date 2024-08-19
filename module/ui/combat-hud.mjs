5d8c2d82221bc24c0c4a85a054988c1ea6f45a79
Brianna A. <16845165+spyrella@users.noreply.github.com>
2024-08-19 08:43:01 -0400

    chore: update compendium

--- module/ui/combat-hud.mjs
+++ module/ui/combat-hud.mjs
@@ -165,7 +165,7 @@ export class CombatHUD extends Application {
 		for (const combatant of game.combat.combatants) {
 			if (!combatant.actor || !combatant.token) continue;
 
-			const activeEffects = (game.release.generation >= 11 ? Array.from(combatant.actor.allApplicableEffects()) : combatant.actor.effects).filter(e => !e.disabled && !e.isSuppressed);
+			const activeEffects = (game.release.generation >= 11 ? Array.from(combatant.actor.allApplicableEffects()) : combatant.actor.effects).filter((e) => !e.disabled && !e.isSuppressed);
 			const actorData = {
 				id: combatant.id,
 				actor: combatant.actor,
