19f6f69b854ac8fd62f6d98346d67e66507de005
Christian Sagel <ckpsm@live.com>
2024-12-14 21:56:12 +0100

    Look up actor skils by fuid

--- module/expressions/expressions.mjs
+++ module/expressions/expressions.mjs
@@ -161,17 +161,17 @@ function evaluateMacros(expression, context) {
 	function evaluateMacro(match, name, params) {
 		const splitArgs = params.split(',');
 		switch (name) {
-			// TODO: Level of named skill owned by actor
 			case `sl`: {
-				const skillName = splitArgs[0].match(/(\w+\s*)+/gm)[0];
-				const skill = context.actor.getSingleItemByName(skillName, 'skill');
+				assertActorInContext(context, match);
+				const skillId = splitArgs[0].match(/(\w+-*\s*)+/gm)[0];
+				const skill = context.actor.getSingleItemByFuid(skillId, 'skill');
 				if (!skill) {
-					throw new Error(`The actor ${context.actor.name} does not have a skill named ${skillName}`);
+					ui.notifications.warn('FU.ChatEvaluateNoSkill', { localize: true });
+					throw new Error(`The actor ${context.actor.name} does not have a skill with the Fabula Ultima Id ${skillId}`);
 				}
 				return skill.system.level.value;
 			}
 			case 'step':
-				assertActorInContext(context, match);
 				return context.actor.system.byLevel.apply(context.actor.system, splitArgs);
 			default:
 				throw new Error(`Unsupported macro ${name}`);
