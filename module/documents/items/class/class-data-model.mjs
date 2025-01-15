f50fa710b1bc31c9c8d9018caf577116aa62896c
Sören Jahns <jahns.simon@web.de>
2025-01-15 21:42:38 +0100

    refactor: Data model cleanup
    - refactor class chat card rendering

--- module/documents/items/class/class-data-model.mjs
+++ module/documents/items/class/class-data-model.mjs
@@ -1,9 +1,43 @@
 import { ClassMigrations } from './class-migrations.mjs';
 import { CheckHooks } from '../../../checks/check-hooks.mjs';
 
+const tagProperties = {
+	'benefits.resources.hp.value': 'FU.BenefitHp',
+	'benefits.resources.mp.value': 'FU.BenefitMp',
+	'benefits.resources.ip.value': 'FU.BenefitIp',
+	'benefits.martials.melee.value': 'FU.BenefitMelee',
+	'benefits.martials.ranged.value': 'FU.BenefitRanged',
+	'benefits.martials.armor.value': 'FU.BenefitArmor',
+	'benefits.martials.shields.value': 'FU.BenefitShields',
+	'benefits.rituals.arcanism.value': 'FU.Arcanism',
+	'benefits.rituals.chimerism.value': 'FU.Chimerism',
+	'benefits.rituals.elementalism.value': 'FU.Elementalism',
+	'benefits.rituals.entropism.value': 'FU.Entropism',
+	'benefits.rituals.ritualism.value': 'FU.Ritualism',
+	'benefits.rituals.spiritism.value': 'FU.Spiritism',
+};
+
 Hooks.on(CheckHooks.renderCheck, (sections, check, actor, item) => {
 	if (item?.system instanceof ClassDataModel) {
-		sections.push(item.createChatMessage(item, false).then((v) => ({ content: v.content })));
+		const tags = Object.entries(tagProperties)
+			.filter(([property]) => foundry.utils.getProperty(item.system, property))
+			.map(([, translation]) => ({ tag: translation }));
+		if (tags.length > 0) {
+			sections.push({
+				partial: 'systems/projectfu/templates/chat/partials/chat-item-tags.hbs',
+				data: { tags },
+			});
+		}
+
+		if (item.system.summary.value || item.system.description) {
+			sections.push(async () => ({
+				partial: 'systems/projectfu/templates/chat/partials/chat-item-description.hbs',
+				data: {
+					summary: item.system.summary.value,
+					description: await TextEditor.enrichHTML(item.system.description),
+				},
+			}));
+		}
 	}
 });
 
