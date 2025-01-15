3ec3eebc1e2a92f0c08d9bb30e15bb25e67a6e58
Sören Jahns <jahns.simon@web.de>
2025-01-15 15:26:07 +0100

    refactor: Data model cleanup
    - refactor accessory chat card rendering
    - clean up accessory data model

--- module/documents/items/accessory/accessory-data-model.mjs
+++ module/documents/items/accessory/accessory-data-model.mjs
@@ -1,8 +1,49 @@
 import { CheckHooks } from '../../../checks/check-hooks.mjs';
+import { deprecationNotice } from '../../../helpers/deprecation-helper.mjs';
 
 Hooks.on(CheckHooks.renderCheck, (sections, check, actor, item) => {
 	if (item?.system instanceof AccessoryDataModel) {
-		sections.push(item.createChatMessage(item, false).then((v) => ({ content: v.content })));
+		const tags = [
+			{
+				tag: 'FU.DefenseAbbr',
+				value: item.system.def.value,
+			},
+			{
+				tag: 'FU.MagicDefenseAbbr',
+				value: item.system.mdef.value,
+			},
+			{
+				tag: 'FU.InitiativeAbbr',
+				value: item.system.init.value,
+			},
+		].filter(({ value }) => value !== 0);
+		if (tags.length > 0) {
+			sections.push({
+				partial: 'systems/projectfu/templates/chat/partials/chat-item-tags.hbs',
+				data: {
+					tags,
+				},
+			});
+		}
+
+		if (item.system.quality.value) {
+			sections.push({
+				partial: 'systems/projectfu/templates/chat/partials/chat-item-quality.hbs',
+				data: {
+					quality: item.system.quality.value,
+				},
+			});
+		}
+
+		if (item.system.summary.value || item.system.description) {
+			sections.push(async () => ({
+				partial: 'systems/projectfu/templates/chat/partials/chat-item-description.hbs',
+				data: {
+					summary: item.system.summary.value,
+					description: await TextEditor.enrichHTML(item.system.description),
+				},
+			}));
+		}
 	}
 });
 
@@ -13,17 +54,20 @@ Hooks.on(CheckHooks.renderCheck, (sections, check, actor, item) => {
  * @property {boolean} isFavored.value
  * @property {boolean} showTitleCard.value
  * @property {number} cost.value
- * @property {boolean} isMartial
  * @property {string} quality.value
  * @property {number} def.value
  * @property {number} mdef.value
  * @property {number} init.value
- * @property {boolean} isBehavior.value
- * @property {number} weight.value
  * @property {string} source.value
- * @property {boolean} rollInfo.useWeapon.hrZero.value
  */
 export class AccessoryDataModel extends foundry.abstract.TypeDataModel {
+	static {
+		deprecationNotice(this, 'isMartial.value');
+		deprecationNotice(this, 'isBehaviour.value');
+		deprecationNotice(this, 'weight.value');
+		deprecationNotice(this, 'rollInfo.useWeapon.hrZero.value');
+	}
+
 	static defineSchema() {
 		const { SchemaField, StringField, HTMLField, BooleanField, NumberField } = foundry.data.fields;
 		return {
@@ -34,19 +78,11 @@ export class AccessoryDataModel extends foundry.abstract.TypeDataModel {
 			isFavored: new SchemaField({ value: new BooleanField() }),
 			showTitleCard: new SchemaField({ value: new BooleanField() }),
 			cost: new SchemaField({ value: new NumberField({ initial: 100, min: 0, integer: true, nullable: false }) }),
-			isMartial: new SchemaField({ value: new BooleanField() }),
 			quality: new SchemaField({ value: new StringField() }),
 			def: new SchemaField({ value: new NumberField({ initial: 0, integer: true, nullable: false }) }),
 			mdef: new SchemaField({ value: new NumberField({ initial: 0, integer: true, nullable: false }) }),
 			init: new SchemaField({ value: new NumberField({ initial: 0, integer: true, nullable: false }) }),
-			isBehavior: new SchemaField({ value: new BooleanField() }),
-			weight: new SchemaField({ value: new NumberField({ initial: 1, min: 1, integer: true, nullable: false }) }),
 			source: new SchemaField({ value: new StringField() }),
-			rollInfo: new SchemaField({
-				useWeapon: new SchemaField({
-					hrZero: new SchemaField({ value: new BooleanField() }),
-				}),
-			}),
 		};
 	}
 
