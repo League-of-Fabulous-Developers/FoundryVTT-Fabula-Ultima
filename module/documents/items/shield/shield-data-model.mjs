165b89ef0afa78980d01571f081c80440382cb44
Sören Jahns <jahns.simon@web.de>
2025-01-29 16:57:22 +0100

    fix: Shields breaking NPC sheet when using NPC equipment

--- module/documents/items/shield/shield-data-model.mjs
+++ module/documents/items/shield/shield-data-model.mjs
@@ -120,6 +120,6 @@ export class ShieldDataModel extends foundry.abstract.TypeDataModel {
 	}
 
 	transferEffects() {
-		return this.parent.isEquipped && !this.parent.actor?.system.vehicle.weaponsActive;
+		return this.parent.isEquipped && !this.parent.actor?.system.vehicle?.weaponsActive;
 	}
 }
