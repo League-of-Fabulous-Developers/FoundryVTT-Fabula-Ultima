88bd1a6f1253be8f3a694cf827b96728cc7484d1
Christian Sagel <ckpsm@live.com>
2025-04-21 20:57:46 +0200

    Set up an auto-complete input for active effect attribute keys (#311)

--- module/documents/effects/active-effect-config.mjs
+++ module/documents/effects/active-effect-config.mjs
@@ -9,14 +9,12 @@ import { FU } from '../../helpers/config.mjs';
  * @param {Record<string, any>} context The object of data used when rendering the application
  */
 export async function onRenderActiveEffectConfig(sheet, html, context) {
-	const data = {
-		effect: sheet.document,
-		system: sheet.document.system,
-		effectDuration: FU.effectDuration,
-		effectType: FU.effectType,
-		effectTracking: FU.effectTracking,
-		crisisInteractions: FU.crisisInteractions,
-	};
+	let data = context.data;
+	data.system = sheet.document.system;
+	data.effectDuration = FU.effectDuration;
+	data.effectType = FU.effectType;
+	data.effectTracking = FU.effectTracking;
+	data.crisisInteractions = FU.crisisInteractions;
 
 	// Effect Type select field (Append)
 	const detailsTemplate = await renderTemplate(`systems/projectfu/templates/effects/active-effect-details.hbs`, data);
@@ -48,10 +46,74 @@ export async function onRenderActiveEffectConfig(sheet, html, context) {
 	const predicateTemplate = await renderTemplate(`systems/projectfu/templates/effects/active-effect-predicate.hbs`, data);
 	durationTab.before(predicateTemplate);
 
-	// Rules Tab (Add)
+	// Effects Tab
 	const effectsTab = html.find('.tab[data-tab=effects]');
+	if (!html.find('#effect-key-options').length) {
+		const options = getAttributeKeys();
+		const datalist = $('<datalist id="effect-key-options"></datalist>');
+		options.forEach((opt) => datalist.append(`<option value="${opt}">`));
+		html.append(datalist);
+	}
+	html.find('.key input').each((index, el) => {
+		const $el = $(el);
+		const name = $el.attr('name');
+		const value = $el.val();
+
+		// Create the new input element (e.g., with a datalist or custom attributes)
+		const $newInput = $(`<input type="text" name="${name}" value="${value}" list="effect-key-options" />`);
+		// Replace the original input with the new one
+		$el.replaceWith($newInput);
+	});
+
+	// Rules Tab (Add)
 	const rulesTemplate = await renderTemplate(`systems/projectfu/templates/effects/active-effect-rules.hbs`, data);
 	effectsTab.before(rulesTemplate);
 
 	sheet.setPosition({ ...sheet.position, height: 'auto' });
+}
+
+let attributeKeys = undefined;
+
+/**
+ * @returns {String[]}
+ */
+function getAttributeKeys() {
+	if (!attributeKeys) {
+		attributeKeys = [];
+		const characterFields = CONFIG.Actor.dataModels.character.schema.fields;
+		if (characterFields) {
+			attributeKeys = attributeKeys.concat(flattenSchemaFields(characterFields, 'system'));
+			// TODO: Derived Keys
+			// Resources
+			for (const res of ['hp', 'mp', 'ip']) {
+				attributeKeys.push(`system.resources.${res}.max`);
+			}
+			// Attributes
+			for (const attr of Object.keys(FU.attributes)) {
+				attributeKeys.push(`system.attributes.${attr}.current`);
+			}
+			// Stats
+			for (const stat of ['def', 'mdef', 'init']) {
+				attributeKeys.push(`system.derived.${stat}.value`);
+			}
+			// Affinities
+			for (const aff of Object.keys(FU.damageTypes)) {
+				attributeKeys.push(`system.affinities.${aff}.current`);
+			}
+		}
+		attributeKeys = attributeKeys.sort((a, b) => b.localeCompare(a));
+	}
+	return attributeKeys;
+}
+
+function flattenSchemaFields(obj, prefix = '', result = []) {
+	for (const [key, value] of Object.entries(obj)) {
+		const path = prefix ? `${prefix}.${key}` : key;
+		if (value.fields) {
+			flattenSchemaFields(value.fields, path, result);
+		} else {
+			result.push(path);
+		}
+	}
+	return result;
 }
