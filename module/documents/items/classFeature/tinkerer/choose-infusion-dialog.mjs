6726349fa3203cdff5dcc773b5bc4b0963eedfb2
Sören Jahns <jahns.simon@web.de>
2025-08-09 16:49:26 +0200

    Fix infusions

--- module/documents/items/classFeature/tinkerer/choose-infusion-dialog.mjs
+++ module/documents/items/classFeature/tinkerer/choose-infusion-dialog.mjs
@@ -26,32 +26,33 @@ async function prompt(infusions) {
 	const data = {
 		infusions: availableInfusions,
 		FU,
+		dialogId: foundry.utils.randomID(),
 	};
 
 	const content = await foundry.applications.handlebars.renderTemplate('/systems/projectfu/templates/dialog/dialog-choose-infusion.hbs', data);
 
-	const selectedInfusion = await new Promise((resolve) => {
-		const dialog = new foundry.applications.api.DialogV2({
-			window: { title: 'FU.ClassFeatureInfusionsDialogTitle' },
-			label: 'FU.Submit',
-			rejectClose: false,
-			content: content,
-			render: (event, dialog) => {
-				dialog.element.querySelectorAll('[data-action=select][data-index]').forEach((element) => {
-					element.addEventListener('click', () => {
-						const category = element.dataset.category;
-						resolve(data.infusions[category][Number(element.dataset.index)] ?? null);
-						dialog.close();
-					});
-				});
-			},
-			close: () => resolve(null),
-			buttons: {},
-		});
-		dialog.render(true);
+	const result = await foundry.applications.api.DialogV2.input({
+		window: { title: game.i18n.localize('FU.ClassFeatureInfusionsDialogTitle') },
+		classes: ['projectfu', 'unique-dialog', 'backgroundstyle'],
+		label: game.i18n.localize('FU.Submit'),
+		rejectClose: false,
+		content: content,
+		ok: {
+			label: 'FU.Confirm',
+		},
+		submit: (result) => {
+			if (result.infusion) {
+				const [rank, index] = result.infusion.split(/:/);
+				result.infusion = infusions[rank][index];
+			}
+		},
 	});
 
-	return selectedInfusion || null;
+	if (!result?.infusion) {
+		ui.notifications.warn('No infusion selected.');
+	}
+
+	return result?.infusion || null;
 }
 
 export const ChooseInfusionDialog = Object.freeze({
