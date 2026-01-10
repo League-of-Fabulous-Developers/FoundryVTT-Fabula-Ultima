2145dec2588f09571d1a4137efdd8370f9c071de
Christian Sagel <ckpsm@live.com>
2026-01-10 18:53:31 +0100

    Add automation for spell mimic

--- module/documents/effects/predicates/species-rule-predicate.mjs
+++ module/documents/effects/predicates/species-rule-predicate.mjs
@@ -5,7 +5,7 @@ import { FU } from '../../../helpers/config.mjs';
 const fields = foundry.data.fields;
 
 /**
- * @property {FUSpeciesKey} species
+ * @property {Set<DamageType>} species
  */
 export class SpeciesRulePredicate extends RulePredicateDataModel {
 	static {
@@ -14,13 +14,18 @@ export class SpeciesRulePredicate extends RulePredicateDataModel {
 
 	static defineSchema() {
 		return Object.assign(super.defineSchema(), {
-			species: new fields.StringField({ initial: 'beast', choices: Object.keys(FU.species) }),
+			selector: new fields.StringField({
+				initial: 'initial',
+				blank: true,
+				choices: Object.keys(FU.targetSelector),
+			}),
+			species: new fields.SetField(new fields.StringField()),
 		});
 	}
 
 	static migrateData(source) {
 		if (source.species.value) {
-			source.species = 'beast';
+			source.species = [source.species.value];
 		}
 		return super.migrateData(source);
 	}
@@ -37,9 +42,11 @@ export class SpeciesRulePredicate extends RulePredicateDataModel {
 	 * @override
 	 */
 	validateContext(context) {
-		for (const target of context.targets) {
-			if (target.actor.type === 'npc') {
-				if (target.actor.system.species.value === this.species) {
+		const selected = context.selectTargets(this.selector);
+		for (const character of selected) {
+			if (character.actor.type === 'npc') {
+				const npcSpecies = character.actor.system.species.value;
+				if (this.species.has(npcSpecies)) {
 					return true;
 				}
 			}
