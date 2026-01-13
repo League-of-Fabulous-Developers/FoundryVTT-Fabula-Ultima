77075eac0d74ca0cca5e158765c1e2d67f562bae
Christian Sagel <ckpsm@live.com>
2026-01-13 23:01:36 +0100

    Have a specific chat message for pressured.

--- module/documents/actors/npc/npc-data-model.mjs
+++ module/documents/actors/npc/npc-data-model.mjs
@@ -75,6 +75,7 @@ Hooks.on('preUpdateActor', async (document, changed) => {
  * @property {string} associatedTherioforms
  * @property {string} description
  * @property {NpcSkillTracker} spTracker
+ * @property {TraitsDataModel} pressurePoints
  */
 export class NpcDataModel extends foundry.abstract.TypeDataModel {
 	static defineSchema() {
