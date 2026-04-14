219f22a8d8f0329e898f2920fabbdbcb73f1a881
Erica Phelps <erica@blackspork.com>
2026-04-14 06:24:39 -0500

    feat:Begin implementing prepareContext; add setting for order by initiative

--- module/ui/combat-hud/base-combat-hud.mjs
+++ module/ui/combat-hud/base-combat-hud.mjs
@@ -1,9 +1,21 @@
-import { SYSTEM } from '../../helpers/config.mjs';
+import { FU, SYSTEM } from '../../helpers/config.mjs';
 import { systemTemplatePath } from '../../helpers/system-utils.mjs';
 import { FUHooks } from '../../hooks.mjs';
 import { SETTINGS } from '../../settings.js';
+import { FUCombat } from '../combat.mjs';
 
 const DEFAULT_THEME_KEY = 'fu-default';
+
+/**
+ * enum flag used to determine how the combat HUD is docked
+ */
+export const HUDDockPositions = Object.freeze({
+	NONE: 0,
+	TOP: 1 << 0,
+	BOTTOM: 1 << 1,
+	LEFT: 1 << 2,
+	RIGHT: 1 << 3,
+});
 
 /** @type {import('./typedefs.mjs').CombatHUDTheme} */
 const knownClasses = {};
@@ -37,6 +49,10 @@ export class BaseCombatHUD extends foundry.applications.api.HandlebarsApplicatio
 			templates: [],
 		},
 	};
+
+	get minWidth() {
+		return 700;
+	}
 
 	/** @type {boolean} */
 	static get shouldBeVisible() {
@@ -90,7 +106,9 @@ export class BaseCombatHUD extends foundry.applications.api.HandlebarsApplicatio
 	}
 
 	/** Handles any sort of clean up that may be needed */
-	destroy() {}
+	destroy() {
+		this.close();
+	}
 
 	/**
 	 * Initializes the combat HUD
@@ -129,17 +147,175 @@ export class BaseCombatHUD extends foundry.applications.api.HandlebarsApplicatio
 
 	_setSizeAndPosition() {
 		// TODO: Handle popping out (both PopOut! module and v14 native method)
+		const pos = this._calculateWindowPosition();
+
+		this.element.style.width = `${pos.width}px`;
+
+		if (pos.top) this.element.style.top = `${pos.top}px`;
+		if (pos.bottom) this.element.style.bottom = `${pos.bottom}px`;
+		if (pos.left) this.element.style.left = `${pos.left}px`;
+		if (pos.right) this.element.style.right = `${pos.right}px`;
+
+		// TODO: Clamp movement to within the visible screen area
+
+		this._applyButtonPosition();
+	}
+
+	/** Handles positioning the control buttons at the top or bottom */
+	_applyButtonPosition() {
+		const positionButtonFromTop = game.settings.get(SYSTEM, SETTINGS.optionCombatHudPositionButton) === 'top';
+		const topClass = `${this._elementClass}__window-button--top`;
+		const bottomClass = `${this._elementClass}__window-button--bottom`;
+
+		const buttons = this.element.querySelectorAll(`${this._elementClass}__window-button`);
+		for (const button of buttons) {
+			if (positionButtonFromTop) {
+				button.classList.add(topClass);
+				button.classList.remove(bottomClass);
+			} else {
+				button.classList.add(bottomClass);
+				button.classList.remove(topClass);
+			}
+		}
+	}
+
+	/**
+	 * Determines the path to the template partial to use to display a given resource
+	 * @param {string} resource
+	 * @returns {string}
+	 */
+	_getResourcePartial(resource) {
+		if (resource === 'none') return null;
+
+		let theme = game.settings.get(SYSTEM, SETTINGS.optionCombatHudTheme).replace('fu-', '');
+		if (theme === 'default') theme = '';
+		else theme = '-' + theme;
+
+		const basePath = 'systems/projectfu/templates/ui/partials/combat-bar-';
+		return basePath + resource + theme + '.hbs';
+	}
+
+	/**
+	 * Retrieve a source image for a combatant.
+	 * Modified from CombatTracker._getCombatantThumbnail()
+	 * @param {Combatant} combatant         The combatant queried for image.
+	 * @returns {Promise<string>}           The source image attributed for this combatant.
+	 * @protected
+	 */
+	async _getCombatantThumbnail(combatant) {
+		if (combatant._videoSrc && !combatant.img) {
+			if (combatant._thumb) return combatant._thumb;
+			return (combatant._thumb = await game.video.createThumbnail(combatant._videoSrc, {
+				width: 200,
+				height: 200,
+			}));
+		}
+
+		return combatant.img ?? CONST.DEFAULT_TOKEN;
+	}
+
+	/**
+	 * Prepares the context for a given combatant to be passed to our handlebars templates
+	 * @param {import('../combatant.mjs').Combatant} combatant
+	 * @returns {Promise<import('./typedefs.mjs').CombatHUDCombatantContext | undefined>} - {@link CombatHUDCombatantContext}
+	 */
+	async _prepareCombatantContext(combatant) {
+		if (!combatant.token || !combatant.actor || combatant.hidden) return;
+		const activeEffects = game.release.generation >= 11 ? Array.from(combatant.actor.temporaryEffects) : combatant.actor.effects.filter((e) => e.isTemporary).filter((e) => !e.disabled && !e.isSuppressed);
+
+		const zeroPower = Object.values(combatant.actor.tracks).find((track) => track.parent?.item?.system?.optionalType === FU.optionalFeatures.zeroPower);
+
+		const trackedResourcePart1 = this._getResourcePartial(game.settings.get(SYSTEM, SETTINGS.optionCombatHudTrackedResource1));
+		const trackedResourcePart2 = this._getResourcePartial(game.settings.get(SYSTEM, SETTINGS.optionCombatHudTrackedResource2));
+		const trackedResourcePart3 = this._getResourcePartial(game.settings.get(SYSTEM, SETTINGS.optionCombatHudTrackedResource3));
+		const trackedResourcePart4 = this._getResourcePartial(game.settings.get(SYSTEM, SETTINGS.optionCombatHudTrackedResource4));
+
+		/** @type {import('./typedefs.mjs').CombatHUDCombatantContext} */
+		const actorData = {
+			id: combatant.id,
+			name: combatant.name,
+			actor: combatant.actor,
+			isOwner: combatant.isOwner,
+			totalTurns: combatant.totalTurns,
+			token: combatant.token,
+			faction: combatant.faction,
+			disposition: combatant.token.disposition,
+			effects: activeEffects,
+			hasEffects: activeEffects.length > 0 && game.settings.get(SYSTEM, SETTINGS.optionCombatHudShowEffects),
+			img: game.settings.get(SYSTEM, SETTINGS.optionCombatHudPortrait) === 'token' ? await this._getCombatantThumbnail(combatant) : combatant.actor.img,
+			trackedResourcePart1,
+			trackedResourcePart2,
+			trackedResourcePart3,
+			trackedResourcePart4,
+			hideTurns: !FUCombat.showTurnsFor(combatant),
+			order: 0,
+			opacity: (game.settings.get(SYSTEM, SETTINGS.optionCombatHudOpacity) ?? 100) / 100,
+			zeropower: zeroPower?.system?.data ?? {
+				current: 0,
+				max: 0,
+			},
+		};
+
+		const barCount = [trackedResourcePart1, trackedResourcePart2, trackedResourcePart3, trackedResourcePart4].filter((item) => !!item).length;
+
+		switch (barCount) {
+			case 1:
+				actorData.rowClass = 'one-bar';
+				break;
+			case 2:
+				actorData.rowClass = 'two-bars';
+				break;
+			case 3:
+			case 4:
+				// TODO: Should this actually separate between three-bars and four-bars?
+				actorData.rowClass = 'three-bars';
+				break;
+			default:
+				actorData.rowClass = 'no-bars';
+		}
+
+		if (actorData.hasEffects) {
+			const maxEffectsBeforeMarquee = 5;
+			const effectsMarqueeDuration = game.settings.get(SYSTEM, SETTINGS.optionCombatHudEffectsMarqueeDuration);
+			actorData.shouldEffectsMarquee = actorData.effects.length > maxEffectsBeforeMarquee && effectsMarqueeDuration < 9000;
+			actorData.effectsMarqueeDuration = effectsMarqueeDuration;
+
+			actorData.marqueeDirection = game.settings.get(SYSTEM, SETTINGS.optionCombatHudEffectsMarqueeMode);
+		}
+
+		return actorData;
 	}
 
 	/**
 	 * @returns {import('./typedefs.mjs').WindowPosition}
 	 */
 	_calculateWindowPosition() {
-		return {
-			left: 0,
+		const hOffset = -5;
+		const [uiLeft, uiMiddle, uiRight, uiBottom] = ['#ui-left', '#ui-middle', '#ui-right', '#hotbar'].map((selector) => document.querySelector(selector));
+
+		// let hudWidth = uiMiddle.clientWidth * uiLeft.clientWidth * .5;
+		// if (hudWidth < this.minWidth) hudWidth = this.minWidth;
+
+		const hudWidth = Math.max(uiMiddle.clientWidth + uiLeft.clientWidth * 0.5 - (uiRight?.clientWidth ?? 0), this.minWidth);
+
+		const draggedPosition = game.settings.get(SYSTEM, SETTINGS.optionCombatHudDraggedPosition);
+		const positionFromTop = game.settings.get(SYSTEM, SETTINGS.optionCombatHudPosition) === 'top';
+
+		const position = {
+			left: draggedPosition?.x ?? uiMiddle.getBoundingClientRect().left,
 			top: 0,
-			width: window.innerWidth,
+			bottom: 0,
+			right: 0,
+			width: hudWidth + hOffset,
 		};
+
+		if (positionFromTop) {
+			position.top = draggedPosition?.y ?? 48;
+		} else {
+			position.bottom = draggedPosition?.y ?? uiBottom.getBoundingClientRect().top;
+		}
+
+		return position;
 	}
 
 	get _elementClass() {
@@ -149,9 +325,62 @@ export class BaseCombatHUD extends foundry.applications.api.HandlebarsApplicatio
 	async _prepareContext(options = {}) {
 		/** @type {import('./typedefs.mjs').CombatHUDRenderContext} */
 		const context = await super._prepareContext(options);
+
 		context.elementClass = this._elementClass;
 		// Only show the pop-out button if the PopOut! module is active, or we're in Foundry v14 or newer
 		context.showPopoutButton = game.modules.get('popout')?.active || game.release.isNewer('14');
+
+		const combat = game.combat;
+		combat.populateData(context);
+
+		context.combatants = [];
+		context.characters = [];
+		context.npcs = [];
+
+		const ordering = game.settings.get(SYSTEM, SETTINGS.optionCombatHudActorOrdering);
+
+		for (const combatant of combat.combatants) {
+			const combatantContext = await this._prepareCombatantContext(combatant);
+			if (!combatantContext) continue;
+
+			context.combatants.push(combatantContext);
+
+			// TODO: Handle other dispositions?
+			switch (combatant.token.disposition) {
+				case foundry.CONST.TOKEN_DISPOSITIONS.FRIENDLY:
+					context.characters.push(combatantContext);
+					break;
+				default:
+					context.npcs.push(combatantContext);
+			}
+
+			let order = 0;
+			const prevOrder = ordering.find((o) => o.tokenId === combatant.token.id);
+			if (prevOrder) {
+				order = prevOrder.order;
+			} else {
+				if (game.settings.get(SYSTEM, SETTINGS.optionCombatHudOrderByInitiative)) {
+					// Kind of hacky but handles getting actors in descending order based on initiative score,
+					// TODO: Ensure it properly handles actors being added after manual ordering
+					order = 100 - (combatant.actor?.system?.derived?.init?.value ?? 0);
+				} else {
+					if (combatant.token.disposition === CONST.TOKEN_DISPOSITIONS.FRIENDLY) order = context.characters.length + 1;
+					else order = context.npcs.length + 1;
+				}
+
+				ordering.push({
+					tokenId: combatant.token.id,
+					order,
+				});
+			}
+			combatantContext.order = order;
+		}
+
+		context.characters.sort((a, b) => a.order - b.order);
+		context.npcs.sort((a, b) => a.order - b.order);
+		context.combatants.sort((a, b) => a.order - b.order);
+
+		console.log('Combat HUD: Context:', context);
 		return context;
 	}
 
--- module/ui/combat-hud/typedefs.mjs
+++ module/ui/combat-hud/typedefs.mjs
@@ -9,6 +9,8 @@
  * @typedef {Object} WindowPosition
  * @property {number} top
  * @property {number} left
+ * @property {number} bottom
+ * @property {number} right
  * @property {number} width
  */
 
@@ -18,4 +20,48 @@
  *
  * @property {string} elementClass
  * @property {boolean} showPopoutButton
+ * @property {boolean} isCompact
+ * @property {number} opacity
+ * @property {string} additionalStyle
+ * @property {string[]} totalTurns
+ * @property {string[]} turnsLeft
+ * @property {boolean} turnStarted
+ * @property {Combatant} combatant
+ * @property {boolean} isGM
+ * @property {object} icons
+ * @property {Record<string, string>} icons
+ *
+ * @property {CombatHUDCombatantContext[]} npcs
+ * @property {CombatHUDCombatantContext[]} characters
+ * @property {CombatHUDCombatantContext[]} combatants - All combatants in the conflict, regardless of disposition
+ */
+
+/**
+ * @typedef {Object} CombatHUDCombatantContext
+ * @property {string} id
+ * @property {string} name
+ * @property {import("../../documents/actors/actor.mjs").Actor} actor
+ * @property {boolean} isOwner
+ * @property {number} totalTurns
+ * @property {import("../../documents/actors/actor.mjs").Token} token
+ * @property {string} faction
+ * @property {Object} effects
+ * @property {string} img
+ * @property {string} trackedResourcePart1
+ * @property {string} trackedResourcePart2
+ * @property {string} trackedResourcePart3
+ * @property {string} trackedResourcePart4
+ * @property {number} opacity
+ * @property {object} zeropower
+ * @property {object} zeropower.progress
+ * @property {number} zeropower.progress.current
+ * @property {number} zeropower.progress.max
+ *
+ * @property {boolean} hasEffects
+ * @property {string} rowClass
+ * @property {boolean} shouldEffectsMarquee
+ * @property {number} effectsMarqueeDirection
+ * @property {string} marqueeDirection
+ * @property {number} order
+ * @property {boolean} hideTurns
  */
