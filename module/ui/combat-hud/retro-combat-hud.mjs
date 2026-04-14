fa9afe27314bcd2caf044851bd7240ffd816718a
Erica Phelps <erica@blackspork.com>
2026-04-14 07:57:19 -0500

    feat:Begin converting retro theme; add tracks to context

--- module/ui/combat-hud/retro-combat-hud.mjs
+++ module/ui/combat-hud/retro-combat-hud.mjs
@@ -1,9 +1,27 @@
+import { systemTemplatePath } from '../../helpers/system-utils.mjs';
 import { BaseCombatHUD } from './base-combat-hud.mjs';
 
 export class RetroCombatHUD extends BaseCombatHUD {
 	static DEFAULT_OPTIONS = {
 		...super.DEFAULT_OPTIONS,
 		classes: [...super.DEFAULT_OPTIONS.classes, 'combat-hud-retro'],
+	};
+
+	static PARTS = {
+		...super.PARTS,
+		hud: {
+			template: systemTemplatePath(`ui/combat-hud/combat-hud-default`),
+			templates: [
+				systemTemplatePath(`ui/partials/combat-bar-exp`),
+				systemTemplatePath(`ui/partials/combat-bar-fp`),
+				systemTemplatePath(`ui/partials/combat-bar-hp`),
+				systemTemplatePath(`ui/partials/combat-bar-ip`),
+				systemTemplatePath(`ui/partials/combat-bar-mp`),
+				systemTemplatePath(`ui/partials/combat-bar-zenit`),
+				systemTemplatePath(`ui/partials/combat-bar-zeropower`),
+				systemTemplatePath(`ui/partials/combat-hud-turn`),
+			],
+		},
 	};
 
 	get _elementClass() {
