ff03530c9fe200c3f68582732175c92103bd0df8
Alexander Wilkinson <awrdp@comcast.net>
2026-05-15 11:26:42 -0700

    Cleanup of fuid conversion code

--- module/documents/items/class-fuid-converter.mjs
+++ module/documents/items/class-fuid-converter.mjs
@@ -2,29 +2,7 @@ import { SYSTEM } from '../../helpers/config.mjs';
 import { Flags } from '../../helpers/flags.mjs';
 import { CompendiumIndex } from '../../ui/compendium/compendium-index.mjs';
 
-function getMatchingClassFuid(className, classes) {
-	if (className) {
-		// Search for a class with the same name. If found, set the skill's class attribute to match its fu-id
-		const classFound = classes.find((classItem) => {
-			return classItem.name === className;
-		});
-		if (classFound?.system?.fuid) {
-			return classFound.system.fuid;
-		}
-
-		// Search for a class with a fuid that matches the slugified attribute.
-		const classNameSlug = game.projectfu.util.slugify(className);
-		const classFoundWithFuid = classes.find((classItem) => {
-			return classItem.system?.fuid === classNameSlug;
-		});
-		if (classFoundWithFuid?.system?.fuid) {
-			return classFoundWithFuid.system.fuid;
-		}
-	}
-	return false;
-}
-
-async function convertClassAttributeFromClassLists(item, actorClasses, worldClasses, compendiumClasses) {
+async function convertClassAttributeFromClassLists(item, classes) {
 	let updates = {
 		flags: {
 			[SYSTEM]: {
@@ -32,7 +10,6 @@ async function convertClassAttributeFromClassLists(item, actorClasses, worldClas
 			},
 		},
 	};
-	//await item.setFlag(SYSTEM, Flags.ClassConverted, true);
 
 	const classAttribute = item.system?.class?.value;
 	if (classAttribute) {
@@ -47,29 +24,13 @@ async function convertClassAttributeFromClassLists(item, actorClasses, worldClas
 			}
 
 			// Search for a class with the same name or fuid and replace its class attribute
-			// Search the actor first
-			if (actorClasses) {
-				const fuid = getMatchingClassFuid(className, actorClasses);
-				if (fuid) {
-					fuids.push(fuid);
-					continue;
-				}
-			}
-
-			// Search the world
-			if (worldClasses) {
-				const fuid = getMatchingClassFuid(className, worldClasses);
-				if (fuid) {
-					fuids.push(fuid);
-					continue;
-				}
-			}
-
-			// Search the compendium
-			if (compendiumClasses) {
-				const fuid = getMatchingClassFuid(className, compendiumClasses);
-				if (fuid) {
-					fuids.push(fuid);
+			if (classes) {
+				// Search for a class with the same name or with a fuid that matches the slugified name. If found, set the skill's class attribute to match its fu-id
+				const classFound = classes.find((classItem) => {
+					return classItem.name === className || classItem.system?.fuid === classNameSlug;
+				});
+				if (classFound?.system?.fuid) {
+					fuids.push(classFound.system.fuid);
 					continue;
 				}
 			}
@@ -77,11 +38,9 @@ async function convertClassAttributeFromClassLists(item, actorClasses, worldClas
 
 		if (fuids.length > 0) {
 			let newClassAttribute = fuids[0];
-
 			for (let i = 1; i < fuids.length; ++i) {
 				newClassAttribute += ', ' + fuids[i];
 			}
-			//await item.update({ 'system.class.value': newClassAttribute });
 			updates['system.class.value'] = newClassAttribute;
 			console.log(`Converted ${item.name} Class Attribute from ${classAttribute} to ${newClassAttribute}`);
 		}
@@ -107,9 +66,10 @@ async function convertActorItemsClassAttributes(actorSource) {
 		return item.type === 'class';
 	});
 	const compendiumClasses = await CompendiumIndex.instance.getClasses();
+	const allClasses = actorClasses.concat(worldClasses, compendiumClasses.class);
 
 	for (const itemToConvert of itemsToConvert) {
-		await convertClassAttributeFromClassLists(itemToConvert, actorClasses, worldClasses, compendiumClasses?.class);
+		await convertClassAttributeFromClassLists(itemToConvert, allClasses);
 	}
 }
 
