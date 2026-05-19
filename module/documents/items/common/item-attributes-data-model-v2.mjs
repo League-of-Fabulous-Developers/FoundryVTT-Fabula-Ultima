99d5f4de145b9fe438bd578f09a93fa789be75f3
Alexander Wilkinson <awrdp@comcast.net>
2026-05-19 11:59:19 -0700

    Fixed Up Migrations for BaseSkillDataModel data

--- module/documents/items/common/item-attributes-data-model-v2.mjs
+++ module/documents/items/common/item-attributes-data-model-v2.mjs
@@ -12,4 +12,19 @@ export class ItemAttributesDataModelV2 extends foundry.abstract.DataModel {
 			secondary: new StringField({ initial: 'dex', blank: true, choices: Object.keys(FU.attributes) }),
 		};
 	}
+
+	static migrateData(source) {
+		source = super.migrateData(source);
+		migrateAttributes(source);
+		return source;
+	}
+}
+
+function migrateAttributes(source) {
+	if (typeof source.primary === 'object' && 'value' in source.primary) {
+		source.primary = source.primary.value ?? 'dex';
+	}
+	if (typeof source.secondary === 'object' && 'value' in source.secondary) {
+		source.secondary = source.secondary.value ?? 'ins';
+	}
 }
