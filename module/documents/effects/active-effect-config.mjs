c4503a5a7232457854263b7d183bd6f89d1120ca
Erica <erica@blackspork.com>
2026-07-13 03:02:14 -0500

    fix:Some CSS updates to ActiveEffect sheet

--- module/documents/effects/active-effect-config.mjs
+++ module/documents/effects/active-effect-config.mjs
@@ -193,6 +193,11 @@ export class FUActiveEffectConfig extends foundry.applications.sheets.ActiveEffe
 		}
 
 		context.expandedRuleElements = this.#expandedRuleElements;
+
+		if (context.fields?.origin instanceof foundry.data.fields.DocumentUUIDField && context.source?.origin) {
+			const originDoc = await fromUuid(context.source.origin);
+			context.originName = originDoc?.name ?? '';
+		}
 		return context;
 	}
 
