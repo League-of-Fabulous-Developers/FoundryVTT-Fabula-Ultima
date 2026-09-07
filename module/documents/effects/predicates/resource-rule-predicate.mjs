a2260d01532191fdd706f9e36de068e2eb12ae56
Erica <erica@blackspork.com>
2026-09-07 16:16:53 -0500

    fix: Implement ResourceRulePredicate; Fixes #1362

--- module/documents/effects/predicates/resource-rule-predicate.mjs
+++ module/documents/effects/predicates/resource-rule-predicate.mjs
@@ -1,6 +1,7 @@
 import { RulePredicateDataModel } from './rule-predicate-data-model.mjs';
 import { systemTemplatePath } from '../../../helpers/system-utils.mjs';
 import { FU } from '../../../helpers/config.mjs';
+import { ComparisonOperations } from '../../../helpers/comparison-operations.mjs';
 
 const fields = foundry.data.fields;
 
@@ -16,6 +17,24 @@ export class ResourceRulePredicate extends RulePredicateDataModel {
 				choices: Object.keys(FU.resources),
 				required: true,
 			}),
+			change: new fields.StringField({
+				initial: '',
+				blank: true,
+				choices: Object.keys(FU.scalarChange),
+			}),
+			changeThreshold: new fields.SchemaField({
+				operator: new fields.StringField({
+					initial: '',
+					blank: true,
+					choices: {
+						max: 'FU.Max',
+						...FU.comparisonOperator,
+					},
+				}),
+				amount: new fields.NumberField({ initial: 0 }),
+			}),
+			local: new fields.BooleanField({ initial: false }),
+			identifier: new fields.StringField({ initial: '' }),
 		};
 	}
 
@@ -31,7 +50,26 @@ export class ResourceRulePredicate extends RulePredicateDataModel {
 	 * @override
 	 */
 	validateContext(context) {
-		// TODO: Implement
+		if (context.origin === context.event.origin || !context.character) return false;
+		if (this.local && !context.isLocalItem()) return false;
+		if (this.identifier && !context.matchesItem(this.identifier)) return false;
+
+		const actor = context.character?.actor;
+
+		if (!actor) return false;
+
+		const { value, max } = actor.system.resources[this.resource];
+		if (value === undefined) return false;
+
+		if (!this.changeThreshold.operator) return false;
+
+		if (this.changeThreshold.operator === 'max') return value >= max;
+
+		if (this.changeThreshold.operator) {
+			const comparisonOperation = ComparisonOperations[this.changeThreshold.operator];
+			if (comparisonOperation(Math.abs(value), Math.abs(this.changeThreshold.amount))) return true;
+		}
+
 		return false;
 	}
 }
